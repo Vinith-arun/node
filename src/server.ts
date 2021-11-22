9013bbea76016bd406edbce2971a6fa7e74c37e0
Vinith-arun <vinith.p@tringapps.com>
2021-11-22 18:37:50 +0530

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,39 @@
-import express from 'express';
+import express, { response } from 'express';
+import { url } from 'inspector';
 const cors=require('cors');
 const app:express.Application=express();
+let jso=[{"name":"vinith"}]
+let vj=[{id:1,name:"Master",price:"20",bought:false,rent:true,daysLeft:"6"},
+    {id:2,name:"Bigil",price:"15",bought:false,rent:false,daysLeft:""}]
+let aj=[
+    {id:5,name:"Vedalam",price:"25" ,bought:false,rent:false,daysLeft:""},
+    {id:6,name:"Vivegam",price:"15",bought:false,rent:false,daysLeft:""},
+    {id:3,name:"Viswasam",price:"15",bought:true,rent:false,daysLeft:""},
+    {id:4,name:"Valimai",price:"15",bought:true,rent:false,daysLeft:""}]
 app.use(cors({
     origin:"http://localhost:3000"
 }))
-const path=require('path');
-const filePath = path.join(__dirname,"../files");
 app.get('/',(req:express.Request, res:express.Response, next:express.NextFunction)=>{
-    res.send('This is homepage');
+    res.send(jso);
+    res.set({"content-type": "text/json; charset=utf-8"});
     res.end()
 });       
 app.get('/second',(req:express.Request, res:express.Response, next:express.NextFunction)=>{
-    res.send('This is secondpage');
+    res.header("Content-Type","text/html");
+    res.sendFile('/font-end/public/index.html');
     res.end()
+});
+app.get('/vijaymovie',(req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    res.send(vj);
+    res.end();
+});
+app.get('/ajithmovie',(req:express.Request, res:express.Response, next:express.NextFunction)=>{
+    res.send(aj);
+    res.end();
 });
 app.listen(9000,()=>{
     console.log('Server running')});
+
+ 
+
 
